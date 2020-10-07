33b0bd7dd6384189ab7f4d4d657e7f979d2a868b
williamkhoo <william_khoo@outlook.com>
2020-10-07 13:58:57 +0800

    update gitignore

--- gpu_cropped.py
+++ gpu_cropped.py
@@ -48,6 +48,13 @@ config = tf.compat.v1.ConfigProto(
          log_device_placement=True
          )
 
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  try:
+    for gpu in gpus:
+      tf.config.experimental.set_memory_growth(gpu, True)
+  except RuntimeError as e:
+    print(e)
 #config.gpu_options.allow_growth = True
 #config.gpu_options.per_process_gpu_memory_fraction = 0.4
 
