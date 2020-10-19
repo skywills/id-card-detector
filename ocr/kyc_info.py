6e1aabdd708ccf237445f7a09b597dc443e2a7dd
williamkhoo <william_khoo@outlook.com>
2020-10-19 21:35:22 +0800

    update

---
+++ ocr/kyc_info.py
@@ -0,0 +1,10 @@
+
+class kycInfo:
+
+    def __init__(self, registerNumber, name, gender, dob=None, address=None):
+        self.registerNumber = registerNumber
+        self.name = name
+        self.gender = gender
+        self.dob = dob
+        self.address = address
+    
---
+++ ocr/my/id_ocr.py
@@ -0,0 +1,11 @@
+
+MIN_HEIGHT = 540
+MIN_WIDTH = 860
+SHAPE_SIZE = (MIN_WIDTH,MIN_HEIGHT)
+
+def extractKycInfo(img):
+    pass
+
+
+def extractRegisterNumber(img):
+    pass
